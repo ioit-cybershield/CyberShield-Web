8a35880c8e686dee53a492e9b0e56a4d4da6978e
Meghraj Jare <meghrajjare77@gmail.com>
2025-12-21 20:34:14 +0530

    setup: initializing workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://ioit-cybershield.github.io",
+  base: "CyberShield-Web",
+});
